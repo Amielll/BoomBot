b453250c2f14cf36925ed777286f62d7eb097847
Kirsten <sutantyo@teach.cs.utoronto.ca>
2024-01-27 17:50:13 -0500

    made login

--- boomboxify/src/App.js
+++ boomboxify/src/App.js
@@ -1,11 +1,8 @@
-import musicnotes from "./musical_notes.png"
 import boombox from "./boombox.png"
-import waves from "./waves.png"
 import './App.css';
 import { useEffect, useState } from "react"
 import SpotifyWebApi from "spotify-web-api-js"
 import Popup from "./popup.js";
-import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition'
 
 function App(props) {
     const setPlaylistActive = props.setPlaylistActive;
@@ -79,22 +76,21 @@ function App(props) {
                 <div id="content-wrap">  
                     <div>{popupToggle ? <Popup handleUserAgeInput={handleUserAgeInput}></Popup> : <p></p>}</div>
                     <div class="my-style">
-                        BOOMBOT
+                        BoomBot
                     </div>
                     <div class='subheader'>
                         Your Retro AI Boombox, Bringing Nostalgia to the Digital Era!
                     </div>
                     <div class="button-container">
                         <a href={loginUrl} class="button-link">
-                            <button id="button1">Get started with Spotify!</button>
+                            <button id="button1">Log in with Spotify!</button>
                         </a>
-                        <button id="button2" onClick={() => handleChatbotButtonClick()}>Talk to our boombot!</button>
                     </div>            
                 </div>
             </div>
             <div class="corner-border top-right"></div>
             <div class="corner-border bottom-left"></div>
-            <img src={boombox} alt="boombox" class="boombox"/>
+            <div class="image-container"><img src={boombox} alt="boombox" class="boombox"/></div>
         </div>
 
     );
