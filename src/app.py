b9d47ae6cab716957773f6a9164682650229efca
chomnk <86968977+chomnk@users.noreply.github.com>
2024-01-26 21:01:30 -0500

    change port number

--- src/app.py
+++ src/app.py
@@ -11,4 +11,4 @@ My excitement level: <|{excitement}|text|>
 """
 excitement = 100
 
-Gui(page=excitement_page).run(use_reloader=True, dark_mode=False)
+Gui(page=excitement_page).run(use_reloader=True, dark_mode=False, port=5001)
