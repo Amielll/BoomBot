b340d95dc3989e2d7b579ddad168fff1e928a7c9
Kirsten <sutantyo@teach.cs.utoronto.ca>
2024-01-26 23:08:45 -0500

    added port number 5001 to development mode

--- src/app.py
+++ src/app.py
@@ -14,7 +14,7 @@ tp_app = Gui(page=excitement_page)
 
 if __name__ == "__main__":
     # Development mode, Flask runs the application for debugging.
-    tp_app.run(use_reloader=True, dark_mode=False)
+    tp_app.run(use_reloader=True, dark_mode=False, port=5001)
 else:
     # Production mode, Azure Web Application runs the application with Gunicorn.
     app = tp_app.run(run_server=False)
