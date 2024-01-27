b4e0e29ebb528cb873e6713426a128b7718707ad
chomnk <86968977+chomnk@users.noreply.github.com>
2024-01-27 03:12:21 -0500

    forgot about taipy use react

--- src/boomboxify.py
+++ src/boomboxify.py
@@ -1,5 +1,26 @@
 from taipy import Gui
+from flask import Flask, redirect
+import multiprocessing
+import time
 
+app = Flask(__name__)
+
+
+AUTH_URL = "https://accounts.spotify.com/authorize"
+
+#@app.route("/")
+def index(state):
+    #state.loginpage = "<a href='/login'>Login with Spotify</a>"
+    eax = redirect(AUTH_URL)
+    print(eax)
+    state.loginpage = redirect(AUTH_URL)
+
+@app.route('/login')
+def login(state=None):
+    return redirect(AUTH_URL)
+
+loginpage = ""
+    
 main_page = """
 
 boomboxify  
@@ -9,9 +30,9 @@ Your Retro AI Boombox, Bringing Nostalgia to the Digital Era!
 {: .subheader}
 
 
-<|get started with spotify|button|id=button1|>
+<|get started with spotify|button|id=button1|on_action=index|>
 
-
+<|{loginpage}|text|>
 <|link to our chatbot!|button|id=button2|>
 
 <|{musical_notes}|image|id=musicnotes|>
@@ -23,4 +44,17 @@ Your Retro AI Boombox, Bringing Nostalgia to the Digital Era!
 musical_notes = './assets/musical_notes.png'
 boombox = './assets/boombox.png'
 
-Gui(page=main_page, css_file='./main.css').run(use_reloader=True, dark_mode=False)
+
+def start_taipy(): 
+    Gui(page=main_page, css_file='./main.css').run(use_reloader=True, dark_mode=False, port=5001)
+
+def start_flask():
+    app.run(port=5000)
+    
+process1 = multiprocessing.Process(target=start_taipy)
+process2 = multiprocessing.Process(target=start_flask)
+
+if __name__ ==  '__main__':
+    process2.start()
+    process1.start()
+
---
+++ src/cohere_test.py
@@ -0,0 +1,12 @@
+import cohere
+co = cohere.Client('BJ6FD9NdGrYA9eS87VokrTMVkwhJhsoonP6dbswF')
+response = co.chat(
+    chat_history=[
+    {"role": "USER", "message": "Can you recommend 2 song that are nostalgic?"},
+    {"role": "CHATBOT", "message": "The man who is widely credited with discovering gravity is Sir Isaac Newton"}
+  ]
+  message="Can you recommend 2 song that are nostalgic?",
+  # perform web search before answering the question. You can also use your own custom connector.
+  connectors=[{"id": "web-search"}]
+)
+print(response.text)
