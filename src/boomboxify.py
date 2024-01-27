c12e445cc25aa2395e2688c828a2c747ec1c4930
David Park <mjdavid.park@mail.utoronto.ca>
2024-01-26 23:15:00 -0500

    Add buttons

--- src/boomboxify.py
+++ src/boomboxify.py
@@ -1,9 +1,19 @@
 from taipy import Gui
 
 main_page = """
+
 boomboxify  
 {: .my-style }
 
+Your Retro AI Boombox, Bringing Nostalgia to the Digital Era!
+{: .subheader}
+
+
+<|get started with spotify|button|id=button1|>
+
+
+<|link to our chatbot!|button|id=button2|>
+
 """
 
-Gui(page=main_page, css_file='./main.css').run(use_reloader=True, dark_mode=False )
+Gui(page=main_page, css_file='./main.css').run(use_reloader=True, dark_mode=False)
