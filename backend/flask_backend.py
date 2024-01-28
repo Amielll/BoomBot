f642b7c2c5981d3f811068e57903400ad84b79ba
chomnk <86968977+chomnk@users.noreply.github.com>
2024-01-27 20:06:46 -0500

    test

---
+++ backend/flask_backend.py
@@ -0,0 +1,81 @@
+from flask import Flask, render_template, request
+import cohere
+from pymongo.mongo_client import MongoClient
+import certifi
+co = cohere.Client('BJ6FD9NdGrYA9eS87VokrTMVkwhJhsoonP6dbswF')
+app = Flask(__name__)
+
+uri = "mongodb+srv://boombot:boombot@cluster0.q6bcgzz.mongodb.net/?retryWrites=true&w=majority"
+# Create a new client and connect to the server
+client = MongoClient(uri, tlsCAFile=certifi.where())
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
+
+@app.route('/api/chat', methods = ['POST'])
+def test():
+    if request.method == "POST":
+        userid = request.form['userid']
+        prompt = request.form['prompt']
+        
+        myDB = client["boombot-db"]
+        chats = myDB.chats
+        users = myDB.users
+        
+        document = chats.find_one({"userid": userid})
+        chat_history = document["chat_history"]
+        
+        data_entry = {}
+        data_entry[userid] = [
+            {"user_name": "User", "text": "Hey!"},
+	        {"user_name": "Chatbot", "text": "Hey! How can I help you today?"}
+        ]
+        
+        print(chat_history)
+        
+        response = co.chat(
+            message=prompt,
+            chat_history=chat_history
+        )
+        
+        # After cohere query, insert user input and cohere response into db, i.e. update chat history.
+        chats.update_one(
+            {"userid": userid},
+            {"$push": {"chat_history": {"user_name": "User", "text": prompt}}}
+        )
+        
+        chats.update_one(
+            {"userid": userid},
+            {"$push": {"chat_history": {"user_name": "Chatbot", "text": response.text}}}
+        )
+        
+        return response.text
+
+
+
+
+@app.route('/api/insert', methods = ['POST'])
+def test_insert():
+    if request.method == "POST":
+        userid = request.form['userid']
+        
+        myDB = client["boombot-db"]
+        chats = myDB.chats
+        users = myDB.users
+        
+        data_entry = {
+            "userid": userid,
+            "chat_history": [
+                {"user_name": "User", "text": "Hey!"},
+	            {"user_name": "Chatbot", "text": "Hey! How can I help you today?"}
+            ]
+        }
+        
+        insert_id = chats.insert_one(data_entry).inserted_id
+        return str(insert_id)
+
+if __name__ == "__main__":
+    app.run()
